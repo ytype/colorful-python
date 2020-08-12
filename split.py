6fce6c94bf988d2122b1b497e3c0b9301b1c824a
권신학 <ytype33@gmail.com>
2020-08-13 02:47:13 +0900

    ENH: update

---
+++ split.py
@@ -0,0 +1,41 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from collections import Counter
+from skimage.color import rgb2lab, deltaE_cie76
+import os
+
+image = cv2.imread('design.jpg')
+
+def RGB2HEX(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def get_colors(image, number_of_colors,show_chart):
+    modified_image = cv2.resize(image, (600, 400), interpolation = cv2.INTER_AREA)
+    modified_image = modified_image.reshape(modified_image.shape[0]*modified_image.shape[1], 3)
+
+    clf = KMeans(n_clusters = number_of_colors)
+    labels = clf.fit_predict(modified_image)
+
+    counts = Counter(labels)
+
+    center_colors = clf.cluster_centers_
+    # We get ordered colors by iterating through the keys
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
+    rgb_colors = [ordered_colors[i] for i in counts.keys()]
+    plt.figure(figsize = (8, 6))
+    plt.pie(counts.values(), labels = hex_colors, colors = hex_colors)
+    plt.show()
+    print(counts.values())
+    return rgb_colors
+
+get_colors(get_image('design.jpg'), 8, True)
+
+#From https://towardsdatascience.com/color-identification-in-images-machine-learning-application-b26e770c4c71
